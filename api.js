2de35f71c3c00dbcbefed56ee42e7a9adb3db2e3
Matthew Sanner <gravityskunk@gmail.com>
2023-08-29 21:25:42 -0600

    setup jest extension

--- api.js
+++ api.js
@@ -1,6 +1,6 @@
 // api.js
 async function fetchData() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/charmander");
   const data = await response.json();
   return data;
 }
